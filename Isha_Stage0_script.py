8b2052902b3f7ad55806a39cf28bd23f0faace8b
ISHA BARVE <80147829+vanillaberryparfait@users.noreply.github.com>
2021-08-02 18:38:07 +0530

    Update Isha_Stage0_script.py

--- Isha_Stage0_script.py
+++ Isha_Stage0_script.py
@@ -1,57 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-name = "Isha Barve"
-
-
-# In[2]:
-
-
-print (name)
-
-
-# In[3]:
-
-
-email = "isha.barve0@gmail.com"
-
-
-# In[4]:
-
-
-print (email)
-
-
-# In[5]:
-
-
-slackname = "@isha"
-
-
-# In[6]:
-
-
-print (slackname)
-
-
-# In[7]:
-
-
-biostack = "Drug Designing"
-
-
-# In[8]:
-
-
-print (biostack)
-
-
-# In[10]:
-
-
 List = [ "NAME = Isha Barve", "EMAIL = isha.barve0@gmail.com" , "SLACK USERNAME = @Isha" , "BIOSTACK = Drug Designing"]
 print (*List, sep = "\n")
 

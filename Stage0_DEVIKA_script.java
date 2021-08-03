4ea472f39f9b050e3eaf3614473568849cf82de9
Devikakaliana <88374155+Devikakaliana@users.noreply.github.com>
2021-08-03 19:21:55 +0530

    Add files via upload

---
+++ Stage0_DEVIKA_script.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Stage0_DEVIKA_script
+{
+           public static void main(String[] args) {
+               String name, email, biostack ;
+               System.out.println("Name = Devika Kaliana : ");
+               System.out.println("Email = devikakaliana94@gmail.com : ");
+               System.out.println("slack username = Devika : ");
+               System.out.println("Biostack = Gemonics : ");
+            }
+}
+        

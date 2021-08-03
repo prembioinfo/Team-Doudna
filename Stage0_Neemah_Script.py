306639f911cd7815e9bb4be49efb22ec52947266
Neemahj <81713579+Neemahj@users.noreply.github.com>
2021-08-03 22:47:34 +0100

    Stage0_ Neemah_Script.py

---
+++ Stage0_Neemah_Script.py
@@ -0,0 +1,6 @@
+@@ -0,0 -1,5 @@
+name: str = "Idowu Jinadu"
+email: str = "neemahjinadu@gmail.com"
+slack_username: str = "@Neemahj"
+  Biostack:   Drug Development
+    print ("{0}, {1}, {2}, {3}" .format (name, email, slack_username, biostack))

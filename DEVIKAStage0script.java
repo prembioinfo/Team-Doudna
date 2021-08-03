37539c083be59d553456becd96479afaa8b4694e
Devikakaliana <88374155+Devikakaliana@users.noreply.github.com>
2021-08-03 15:31:47 +0530

    Add files via upload

---
+++ DEVIKAStage0script.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devikastage0script;
+
+/**
+ *
+ * @author Isha
+ */
+public class DEVIKAStage0script {
+
+    public static void main(String[] args) {
+	    String name, email;
+	    System.out.println("Name= Devika Kaliana: ");
+	    System.out.println("Email= devikakaliana94@gmail.com: ");
+	    System.out.println("slack username= Devika :");
+	    System.out.println("Biostack= Genomics :");
+    /**
+     * @param args the command line arguments
+     */
+    
+    }
+    
+}

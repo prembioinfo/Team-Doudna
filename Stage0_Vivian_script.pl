86d221eeb3f51daa663dadc25e1152ea8c9e4240
Vivian Chu <33632082+vivianschu@users.noreply.github.com>
2021-08-03 16:14:34 -0400

    Create Stage0_Vivian_script.pl

---
+++ Stage0_Vivian_script.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $name = 'Vivian Chu';
+my $email = 'vivian.chu@uwaterloo.ca';
+my $slack = 'vivianschu';
+my $biostack = 'Genomics';
+
+my $info = <<"END";
+Name: $name
+Email ID: $email
+Slack Username: $slack
+Biostack: $biostack
+END
+
+print $info
+
+#Program_using_Perl

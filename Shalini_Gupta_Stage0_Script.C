ff2e69f42db6e70b83eb32b3141632a81aa6b432
Shalu03 <88299393+Shalu03@users.noreply.github.com>
2021-08-03 10:01:06 +0530

    Add files via upload

---
+++ Shalini_Gupta_Stage0_Script.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Name: Shalini\n");
+printf("Email Id: shalinigupta099@gmail.com\n");
+printf("Slack Username: @Shalini\n");
+printf("Biostack: Data analytics and Vaccin Informatics\n");
+getch();
+}
+

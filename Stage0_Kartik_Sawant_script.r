7a779d541ba7dc4027bb42241ccfa9508e825ec8
WinterSoldier14 <88366944+WinterSoldier14@users.noreply.github.com>
2021-08-03 16:12:25 +0530

    Add files via upload

---
+++ Stage0_Kartik_Sawant_script.r
@@ -0,0 +1,12 @@
+vara<- "Name : Kartik Atul Sawant"
+
+varb<- "Email : kartikatulsawant14@gmail.com"
+
+varc<- "Slack username : @Kartik Sawant"
+
+vard<- "Biostack : Genomics"
+
+print(vara)
+print(varb)
+print(varc)
+print(vard)

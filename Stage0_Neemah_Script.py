496ff099fbce040b166ef3a335e0e88a4759f5e9
Neemahj <81713579+Neemahj@users.noreply.github.com>
2021-08-04 11:37:28 +0100

    Update Stage0_Neemah_Script.py

--- Stage0_Neemah_Script.py
+++ Stage0_Neemah_Script.py
@@ -1,6 +1,7 @@
-@@ -0,0 -1,5 @@
+#!/usr/local/bin/python3
 name: str = "Idowu Jinadu"
 email: str = "neemahjinadu@gmail.com"
 slack_username: str = "@Neemahj"
 biostack = "Drug Development"
-print ("{0}, {1}, {2}, {3}" .format (name, email, slack_username, biostack))
+twitter_username: "@Neemahj"
+print ("{0}, {1}, {2}, {3}, {4}" .format (name, email, slack_username, biostack, twitter_username))

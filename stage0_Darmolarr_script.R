c50fff28f29d09e1b65c125adac8393fa3016aed
Darmolarr <88286369+Darmolarr@users.noreply.github.com>
2021-08-04 19:20:13 +0100

    Add files via upload

---
+++ stage0_Darmolarr_script.R
@@ -0,0 +1,19 @@
+ vara<- "Name : Salami Habeeb"
+
+ varb<- "Email : damolamillz@gmail.com"
+
+ varc<- "Slack username : @Darmolarr"
+
+ vard<- "Biostack : Microbial Genomics"
+ 
+ vare<- "Twitter username : @Darmolar1"
+ 
+ varf<- "Hamming distance : 1"
+
+
+ print(vara)
+ print(varb)
+ print(varc)
+ print(vard)
+ print(vare)
+ print(varf)

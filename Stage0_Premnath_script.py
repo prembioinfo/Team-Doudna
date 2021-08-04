7bbaf278beb9a187debf39d012049b598d02d255
prembioinfo <88283469+prembioinfo@users.noreply.github.com>
2021-08-04 13:33:02 +0530

    Delete Stage0_Premnath_script.py

--- Stage0_Premnath_script.py
+++
@@ -1,6 +0,0 @@
-Name = "Premnath"
-Email = "batchaamadan17@gmail.com"
-SLACK_USERNAME = "@Premnath"
-Biostack = "Drug Development"
-
-print(f"Name: {Name}\nEmail-ID: {Email}\nSlack Handle: {SLACK_USERNAME}\nBiostack: {Biostack}")

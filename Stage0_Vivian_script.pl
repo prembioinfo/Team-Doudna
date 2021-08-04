78e4d5eac81ec38416df932357d84232f657151b
Vivian Chu <33632082+vivianschu@users.noreply.github.com>
2021-08-04 13:10:07 -0400

    Update Stage0_Vivian_script.pl

--- Stage0_Vivian_script.pl
+++ Stage0_Vivian_script.pl
@@ -6,12 +6,24 @@ my $name = 'Vivian Chu';
 my $email = 'vivian.chu@uwaterloo.ca';
 my $slack = 'vivianschu';
 my $biostack = 'Genomics';
+my $twitter = 'vivianschu';
+
+sub hd {
+        my ($k, $l) = @_;
+        my $diff = $k ^ $l;
+        my $num_mismatch = $diff =~ tr/\0//c;
+        print $diff
+}
+
+my $hamming_distance = hd($slack,$twitter);
 
 my $info = <<"END";
 Name: $name
 Email ID: $email
 Slack Username: $slack
 Biostack: $biostack
+Twitter: \@$twitter
+Hamming Distance: $hamming_distance
 END
 
 print $info

db435a643e60baf556585957636f86c7fc2a7728
prembioinfo <88283469+prembioinfo@users.noreply.github.com>
2021-08-04 11:36:20 +0530

    Add files via upload

---
+++ Stage0_Premnath_script.py
@@ -0,0 +1,6 @@
+Name = "Premnath"
+Email = "batchaamadan17@gmail.com"
+SLACK_USERNAME = "@Premnath"
+Biostack = "Drug Development"
+
+print(f"Name: {Name}\nEmail-ID: {Email}\nSlack Handle: {SLACK_USERNAME}\nBiostack: {Biostack}")

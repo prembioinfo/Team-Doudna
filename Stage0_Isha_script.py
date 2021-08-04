0a50c0ba3731748e0cda1f75ed9b4cd03dc5da01
ISHA BARVE <80147829+vanillaberryparfait@users.noreply.github.com>
2021-08-04 17:31:50 +0530

    Update Stage0_Isha_script.py

--- Stage0_Isha_script.py
+++ Stage0_Isha_script.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-List = [ "NAME = Isha Barve", "EMAIL = isha.barve0@gmail.com" , "SLACK USERNAME = @Isha" , "BIOSTACK = Drug Development"]
-print (*List, sep = "\n")
+NAME = "Isha Barve"
+EMAIL = "isha.barve0@gmail.com"
+SLACKID = "@Isha"
+BIOSTACK = "Drug Development"
+TWITTER = "@isba"
 
+def Hamming_distance(str1, str2):
+    i = 0
+    count = 0
+
+    while (i < len(str1)):
+        if (str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
+
+str1 = SLACKID
+str2 = TWITTER
+
+print(f"NAME:- {NAME}\nEMAIL-ID:- {EMAIL}\nSLACK ID:- {SLACKID}\nBIOSTACK:- {BIOSTACK}\nTWITTER HANDLE:- {TWITTER}\nHAMMING DISTANCE:- {Hamming_distance(str1, str2)}")
 
 # In[ ]:
 

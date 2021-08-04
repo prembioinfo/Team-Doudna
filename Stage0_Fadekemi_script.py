ae78b4da98d1b96071cf25a637248eaa5d7925a4
MorenikejiA <88286518+MorenikejiA@users.noreply.github.com>
2021-08-04 05:56:10 -0500

    Create Stage0_Fadekemi_script.py

---
+++ Stage0_Fadekemi_script.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+name: str = "Adeniran Fadekemi"
+email: str = "fadekeadeniran@yahoo.com"
+slack_username: str = "@Morenike"
+biostack = "Drug Development"
+print ("{0}, {1}, {2}, {3}" .format (name, email, slack_username, biostack))

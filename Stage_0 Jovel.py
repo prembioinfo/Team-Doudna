bced822b4feb876af653801ca561dd6969ee60ee
Jovel Varghese Jose <88280509+MicroboyJovel@users.noreply.github.com>
2021-08-05 14:12:11 +0530

    Update Stage_0 Jovel.py

--- Stage_0 Jovel.py
+++ Stage_0 Jovel.py
@@ -7,15 +7,11 @@ Twitter_handle = "gravityboyjovel"
 def Hamming_distance(str1, str2):
     i = 0
     count = 0
-
     while (i < len(str1)):
         if (str1[i] != str2[i]):
             count += 1
         i += 1
     return count
-
 str1 = Slack_username
 str2 = Twitter_handle
-
-
 print(f"Name: {Name}\n Email: {Email}\n Slack_username: {Slack_username}\n Biostack: {Biostack}\n Twitter_handle : {Twitter_handle }\n Hamming_distance: {Hamming_distance(str1, str2)}")

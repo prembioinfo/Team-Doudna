ca7b742737e29fd0b7f8fa67970aa6bec0c4ab41
ISHA BARVE <80147829+vanillaberryparfait@users.noreply.github.com>
2021-08-05 12:35:05 +0530

    Add files via upload

---
+++ Stage0_DEVIKA_script.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Stage0_DEVIKA_script
+{
+          
+static int hammingDist(String SU, String TH)
+{
+    int i = 0, count = 0;
+    while (i < SU.length())
+    {
+        if (SU.charAt(i) != TH.charAt(i))
+            count++;
+        i++;
+    }
+    return count;
+}
+ 
+public static void main (String[] args)
+{
+    String SU = "@Devika";
+    String TH = "@Devpoo";
+ 
+    
+    System.out.println("HammingDist:"+ hammingDist (SU, TH));
+    System.out.println("Name = Devika Kaliana");
+    System.out.println("Email = devikakaliana94@gmail.com");
+    System.out.println("slack username = @Devika");
+    System.out.println("Biostack = Gemonics");
+    System.out.println("Twitter Handel = @Devpoo");
+}
+}
+              

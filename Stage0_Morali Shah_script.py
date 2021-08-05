f48555fb31d50fe183e1c485257496c1f56e613c
MoraliShah <86672064+MoraliShah@users.noreply.github.com>
2021-08-05 13:17:35 +0530

    Update Stage0_Morali Shah_script.py

--- Stage0_Morali Shah_script.py
+++ Stage0_Morali Shah_script.py
@@ -14,5 +14,21 @@ print (*List, sep = "\n")
 # In[ ]:
 
 
+def hammingDist(str1, str2):
+    i = 0
+    count = 0
+ 
+    while(i < len(str1)):
+        if(str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
+str1 = "Monica"
+str2 = "morali"
+
+print(hammingDist(str1, str2))
+
+
+# In[ ]:
 
 

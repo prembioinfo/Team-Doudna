ed344f0303cdd993c0169dde4a231c741dbc2c91
Jovel Varghese Jose <88280509+MicroboyJovel@users.noreply.github.com>
2021-08-05 12:32:02 +0530

    Add files via upload

---
+++ Jovel.py
@@ -0,0 +1,5 @@
+print("name: Jovel Varghese Jose")
+print("email: jovelvarghesejose718@gmail.com")
+print("slack username@biostack: jovel@transcriptomics")
+print("twitter handle: gravityboyjovel")
+print("hamming distance: 5")

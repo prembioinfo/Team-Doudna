b88263e11f4a9a823ea2f229f5e73c2408fc9d10
Premnath <88283469+prembioinfo@users.noreply.github.com>
2021-08-05 12:11:06 +0530

    Delete sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-Name = "Premnath"
-Email = "batchaamadan17@gmail.com"
-SLACK_USERNAME = "@Premnath"
-Biostack = "Drug Development"
-Twitter_handle = "@Batchaa1"
-Hamming = 7
-
-print(f"Name: {Name}\nEmail-ID: {Email}\nSlack_username: {SLACK_USERNAME}\nBiostack: {Biostack}\nTwitter handle: {Twitter_handle}\nHamming distance: {Hamming}")

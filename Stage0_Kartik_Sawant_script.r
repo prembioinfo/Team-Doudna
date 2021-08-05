13a4ae30096593a178582feda928402a09301153
Kartik Sawant <88366944+WinterSoldier14@users.noreply.github.com>
2021-08-05 17:22:42 +0530

    Add files via upload

--- Stage0_Kartik_Sawant_script.r
+++ Stage0_Kartik_Sawant_script.r
@@ -1,12 +1,41 @@
-vara<- "Name : Kartik Atul Sawant"
+vara<- "Kartik Atul Sawant"
 
-varb<- "Email : kartikatulsawant14@gmail.com"
+varb<- "kartikatulsawant14@gmail.com"
 
-varc<- "Slack username : @Kartik Sawant"
+varc<- "@Kartik"
 
-vard<- "Biostack : Genomics"
+vard<- "Genomics"
 
-print(vara)
-print(varb)
-print(varc)
-print(vard)
+vare<- "@Kartic"
+
+
+varg<- "@Kartik"
+varh<- "@Kartic"
+
+
+HD <- function(str1, str2){
+  
+  str1 <- as.character(str1)
+  str2 <- as.character(str2)
+  
+  length.str1 <- nchar(str1)
+  length.str2 <- nchar(str2)
+  
+  string.temp1 <- c()
+  for (i in 1:length.str1){
+    string.temp1[i] = substr(str1, start=i, stop=i)
+  }
+  string.temp2 <- c()
+  for (i in 1:length.str2){
+    string.temp2[i] = substr(str2, start=i, stop=i)
+  }
+  return(sum(string.temp1 != string.temp2))
+}
+
+hamming <- HD(varg,varh)
+
+# sum(as.vector(varh) != as.vector(varg))
+
+varl<- c(vara,varb,varc,vard,vare,hamming)
+
+cat(paste(varl, collapse = ','))

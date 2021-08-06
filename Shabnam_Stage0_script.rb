3c1d977c7a0d9f5bc3998879e576963d9e3163f4
Shabnam <88210425+Shab-ss@users.noreply.github.com>
2021-08-06 14:04:33 +0530

    Add files via upload

---
+++ Shabnam_Stage0_script.rb
@@ -0,0 +1,22 @@
+#Hackbio_Stage0_My_details
+
+Name="Shabnam S";
+Email_id="shab31201@gmail.com";
+Slack_username="@Shabnam";
+Twitter_username="@Shabs3s";
+Biostack="Genomics";
+
+def hamming(string1,string2)
+     str1=string1.downcase.split('')
+     str2=string2.downcase.split('')
+     hamming_count=0
+     str1.each_index do |i| 
+        hamming_count +=1 if str1.values_at(i)!=str2.values_at(i)
+    end
+    hamming_count
+end
+
+Hamming_distance=hamming("#{Slack_username}","#{Twitter_username}")
+puts "#{Name}, #{Email_id}, #{Slack_username}, #{Twitter_username}, #{Biostack}, #{Hamming_distance}"
+
+#Program_using_ruby

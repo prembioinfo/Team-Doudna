7b439c38afdd3de2b8fab34aad09ac715c6fd410
Kartik Sawant <88366944+WinterSoldier14@users.noreply.github.com>
2021-08-06 20:22:03 +0530

    Add files via upload

--- Stage0_Kartik_Sawant_script.r
+++ Stage0_Kartik_Sawant_script.r
@@ -1,41 +1,9 @@
-vara<- "Kartik Atul Sawant"
+name<- "Kartik Sawant"
+email<- "kartikatulsawant14@gmail.com"
+slack<- "@kartik"
+biostack<- "Genomics"
+twitter<- "kartic"
 
-varb<- "kartikatulsawant14@gmail.com"
+hamming_distance<- sum(slack != twitter)
 
-varc<- "@Kartik"
-
-vard<- "Genomics"
-
-vare<- "@Kartic"
-
-
-varg<- "@Kartik"
-varh<- "@Kartic"
-
-
-HD <- function(str1, str2){
-  
-  str1 <- as.character(str1)
-  str2 <- as.character(str2)
-  
-  length.str1 <- nchar(str1)
-  length.str2 <- nchar(str2)
-  
-  string.temp1 <- c()
-  for (i in 1:length.str1){
-    string.temp1[i] = substr(str1, start=i, stop=i)
-  }
-  string.temp2 <- c()
-  for (i in 1:length.str2){
-    string.temp2[i] = substr(str2, start=i, stop=i)
-  }
-  return(sum(string.temp1 != string.temp2))
-}
-
-hamming <- HD(varg,varh)
-
-# sum(as.vector(varh) != as.vector(varg))
-
-varl<- c(vara,varb,varc,vard,vare,hamming)
-
-cat(paste(varl, collapse = ','))
+cat(name,",",email,",",slack,",",biostack,",",twitter,",",hamming_distance)

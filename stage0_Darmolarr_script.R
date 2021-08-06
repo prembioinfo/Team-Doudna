323505bff8de9e48720767582b83661438b58a2f
Premnath <88283469+prembioinfo@users.noreply.github.com>
2021-08-06 21:29:16 +0530

    Delete stage0_Darmolarr_script.R

--- stage0_Darmolarr_script.R
+++
@@ -1,9 +0,0 @@
-vara<- "Salami Habeeb"
-varb<- "damolamillz@gmail.com"
-varc<- "@Darmolarr"
-vard<- "Microbial Genomics"
-vare<- "@Darmolar0"
-
-HD<- sum(varc != vare)
-
-message(vara,",",varb,",",varc,",",vard,",",vare,",",HD)

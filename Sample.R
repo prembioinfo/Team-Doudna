164f5719259a70a9a742a72226039fb2953e66a0
Premnath <88283469+prembioinfo@users.noreply.github.com>
2021-08-06 21:43:49 +0530

    Delete Sample.R

--- Sample.R
+++
@@ -1,9 +0,0 @@
-vara<- "Salami Habeeb"
-varb<- "damolamillz@gmail.com"
-varc<- "@Darmolarr"
-vard<- "Microbial Genomics"
-vare<- "@Darmolar0"
-
-Ham_Dist<- sum(varc != vare)
-
-cat(vara,",",varb,",",varc,",",vard,",",vare,",",Ham_Dist)
